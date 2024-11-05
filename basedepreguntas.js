ca711a31f0febd68af07445f1b83b9ef7a2cfd4c
davidaliri <davidaliri95@gmail.com>
2024-11-05 12:52:35 -0500

    Add files via upload

---
+++ basedepreguntas.js
@@ -0,0 +1,152 @@
+basedepreguntas = [
+    {
+
+      pregunta: `Lee el texto y  Responde las siguientes Preguntas.`,
+      imagen: `https://i.ibb.co/XSsvYpx/Cam-Scanner-04-11-2024-09-39-page-0001-1.jpg`,
+      respuesta: `	Estas flores se parecen a ti.		`,
+      distractores: [`		Guarda estas flores lindas.	`, `	Estas hermosas flores son para ti.  		`, `	Las violetas son modestas como tú.`],
+      
+      
+    }
+    ,
+    {
+      imagen: `` ,
+      pregunta: `2.	¿Cuál es la causa para que llore Margarita? `,
+      respuesta: `	El ramillete, elogio y amor ofrecidos por Manuel.			`,
+      distractores: [`	La actitud de Manuel de ofrecerle flores.		`, `	La tristeza que sentía en ese momento.	 	`, `	La agilidad de margarita de esconder flores.		`],
+      ayuda: ``,
+      ayudaImg: ``,
+    }
+    ,
+    {
+      imagen: `` ,
+      pregunta: `3.	Según el texto, ¿cómo caracterizas a Margarita?`,
+      respuesta: `	Como una adolescente tímida.		`,
+      distractores: [`	Corno una joven atrevida. 		`, `	Como una joven precavida.	`, `	Como una adolescente extrovertida.		`],	
+        
+        
+       
+    }
+    ,
+    {
+      imagen: `` ,
+      pregunta: `4.	¿Crees que alteraría la secuencia lógica de la historia, si Margarita rechazaba el ramillete que le ofrece Manuel?	`,
+      respuesta: `	Si, definitivamente. Rechazar el ramillete sería un claro indicio de que Margarita no está interesada en Manuel, alterando la secuencia lógica de la historia y una trama diferente.	 	`,
+      distractores: [`No. Porque es un detalle menor. El ramillete es solo un detalle menor en la historia y su rechazo no tendría un impacto significativo en la secuencia lógica de la trama principal.			`, `	Si, pero solo temporalmente. El rechazo del ramillete podría crear un obstáculo temporal en la relación entre Margarita y Manuel, pero la trama podría volver a su curso original. 			`, `		No, no creo. El rechazo del ramillete no cambiaría la esencia de la historia porque ella podría tener razones personales para rechazar e' ramillete y no cambiaría la secuencia.			`],
+    }
+    ,
+    {
+      imagen: ` https://i.ibb.co/x5DxNR9/fjgnvm-cmv-page-0001.jpg` ,
+      pregunta: `Lee el texto y  Responde las siguientes Preguntas.`,
+      respuesta: `		Los muchachos que escogían restos de comida.	`,
+      distractores: [`		Los canillitas que reparten los diarios a las casas		`, `	Las beatas que estaban en la iglesia de la ciudad.			`, `	Los noctámbulos que habían dormido en la noche.	 	`],
+    }
+    ,
+    {
+      imagen: `` ,
+      pregunta: `6.	Lee el siguiente fragmento del texto: A las seis de la mañana la ciudad se levanta de puntillas y comienza a dar sus primeros pasos Una fina niebla disuelve el perfil de los objetos y crea como una atmósfera encantada.[…]
+A esta hora se ve también obreros caminando hacia el tranvía, policías bostezando contra los árboles, canillitas morados de frío, sirvientas sacando los cubos de basura. A esta hora, por último, como a una especie de misteriosa consigna, aparecen los gallinazos sin plumas.
+...¿Cuál es el propósito del autor en los fragmentos del texto? `,
+      respuesta: `	 Establecer el tono y la atmósfera de la historia.		`,
+      distractores: [`		Presentar la condición económica de la población.	`, `	Describir a la ciudad ya las personas importantes,		`, `		Crear una sensación de miedo y suspenso en el lector. 	`],
+    }
+    ,
+    {
+      imagen: `` ,
+      pregunta: `7.	¿A quiénes se refiere el autor cuando dice "Los Gallinazos Sin Plumas"? `,
+      respuesta: `	A las personas más pobres de la ciudad, luchando para sobrevivir 	 `,
+      distractores: [`		A las beatas religiosas y políticos del contexto histórico - social 		`, `	A las aves de la costa que temprano merodeaban por las calles.	 	`, `	A los obreros entusiastas que trepaban las escaleras fijadas.	 	`],
+    }
+    ,
+    {
+      imagen: `` ,
+      pregunta: ` 8.	¿Qué opinas de la estrategia discursiva que emplea el autor para describir a los personales del texto?`,
+      respuesta: `	Pienso que usa metáforas y descripciones objetivas creando una imagen vivida de los personajes.			`,
+      distractores: [`	Opino que el autor utiliza un tono emotivo y sentimental en la historia para evocar lástima en el lector.		`, `		Me parece que el autor del texto quiere presentar a los personajes como figuras heroicas y admirables.		`, `	Creo que el autor de la narración utiliza un lenguaje técnico y especializado para describir sus habilidades.		`],
+    }
+    ,
+    {
+      imagen: ` https://i.ibb.co/hCY2kc1/tyrt-page-0001.jpg` ,
+      pregunta: `Lee el texto y  Responde las siguientes Preguntas. `,
+      respuesta: `		Primera   	 `,
+      distractores: [`		Segunda			`, `	Tercera	 		`, `	Cuarta		 	`],
+    }
+    ,
+    {
+      imagen: `` ,
+      pregunta: `1O. ¿Cuál es la consecuencia de colocar los postes de madera? `,
+      respuesta: `		Proveer la aireación de las capas del compostero, al retirarlos.	`,
+      distractores: [`	Garantizar la hidratación de todas las capas del compost.		`, `	Aumentar considerablemente la alta calidad del material.			`, `	Dejar como elementos decorativos de la poza de compost.			`],
+    }
+    ,
+    {
+      imagen: `` ,
+      pregunta: `11.	¿De qué trata principalmente el texto? `,
+      respuesta: `		De la forma de elaborar el compost para obtener material orgánico enriquecido.	`,
+      distractores: [`	Del proceso de reciclado de los residuos sólidos que generamos diariamente.		`, `	De la manera de usar el compost en los cultivos ecológicos de mucha demanda.	`, `	De las medidas que debe tener una poza de compost para garantizar la cantidad.		`],
+    }
+    ,
+    {
+      imagen: `` ,
+      pregunta: `12.	¿Qué opinas de la infografía "los residuos que forman parte o no del compost" que acompaña al texto? `,
+      respuesta: `		 Creo que la infografía complementa con información importante al texto.	 	`,
+      distractores: [`	Opino que el texto no indica todos los materiales ni procedimientos.			`, `	Me parece que no ayuda a nuestro conocimiento del compost en el hogar.		 	`, `	No me parece porque el texto ya indica los materiales necesarios.			`],
+    }
+    ,
+    {
+      imagen: `	https://i.ibb.co/59Cczj8/fhwerhkfr-page-0001.jpg`,
+      respuesta: `Lee el texto y  Responde las siguientes Preguntas.	` ,
+      pregunta: `	Siri solo entiende el idioma, no el sentido de las frases. `,
+      distractores: [`	Sin interpreta lo que una persona quiere decir y sentir.			`, `	Siri comprende la situación emocional de los clientes.		`, `		 Siri entiende el sentido y significado de las frases.		`],
+    }
+    ,
+    {
+      imagen: `` ,
+      pregunta: `14. ¿Cuál de las alternativas contiene el subtema del segundo párrafo del texto?	`,
+      respuesta: `	El procesamiento e interpretación del lenguaje de las personas.		`,
+      distractores: [`	La Información del macroproyecto denominado asistente de Apple.`, `		La atención al cliente personal del Banco Santander y Vodafone.		`, `		La contratación de un asistente para mejorar la atención al cliente.	`],
+    }
+    ,
+    {
+      imagen: `` ,
+      pregunta: `15.	¿Cuál es el propósito comunicativo del texto leído?	`,
+      respuesta: `	Informar cómo se está trabajando en nuevas investigaciones sobre inteligencia artificial.			`,
+      distractores: [`		Enseñar la potencialidad de iMind Technology, un proyecto informático novedoso.		`, `		Publicitar la empresa tecnológica Full on Net, que ha desarrollado un sistema informático.		`, `	Anunciar sobre el imperio de los datos (big data) anunciado por del Grupo Watson		`],
+    }
+    ,
+    {
+      imagen: `` ,
+      pregunta: `16.	El texto leído inicia con la siguiente expresión: •  un pastor aleman puede ser un perro o un señor que tiene ovejas: ' el análisis del contexto nos permite comprender la diferencia entre ambos significados...........................................................................................¿Qué opinas sobre esta expresión? `,
+      respuesta: `		Opino que la expresión logra llamar la atención del lector con bastante eficacia.		`,
+      distractores: [`	Entiendo que no tiene relación con el tema del texto, ni tiene efecto en el lector.	`, `	Creo que tiene un inicio gracioso, pero el texto no trata del pastor alemán.		`, `	Parece que puede tener varias interpretaciones, depende el contexto de su uso. 		`],
+    }
+    ,
+    {
+      imagen: `https://i.ibb.co/Z8bgvq6/jgrk-page-0001.jpg` ,
+      pregunta: `Lee el texto y  Responde las siguientes Preguntas. `,
+      respuesta: `La presencia frecuente del fenómeno de El Niño requiere planes de acción y coordinaciones con las autoridades.		 	`,
+      distractores: [`	  El registro de 25 episodios del fenómeno de El Niño en nuestro país en los últimos 15 años en las regiones.	`, `	La permanente coordinación intersectorial del gobierno central, así como las autoridades locales y regionales.		`, `	El Fenómeno de El Niño llegó a nuestro país hace 40 mil años; es decir, los moches y nazcas ya lo enfrentaban.	`],
+    }
+    ,
+    {
+      imagen: `` ,
+      pregunta: `18.	Lee siguiente párrafo : Resulta increible constatar que hay gobierno locales y regionales que no ejecutaron el presupuesto asignado para emergencias, prevención, MITIGACION y preparación frente al fenomeno natural.	`,
+      respuesta: `	Reducción		`,
+      distractores: [`	Anticipacion	 		`, `Ejecución				`, `	Evaluación		`],
+    }
+    ,
+    {
+      imagen: `` ,
+      pregunta: `19.	Considerando el texto leído, ¿cuál es una conclusión del texto?`,
+      respuesta: `		La mayoría de los gobiernos regionales no ejecutan la totalidad del presupuesto a las acciones de previsión ante El Niño.		`,
+      distractores: [`	Los planes de acción ejecutadas por las autoridades locales, regionales y nacionales han cumplido con los objetivos,			`, `Las autoridades de los diferentes niveles de gobierno siempre han mostrado disposición, pero no tienen recursos.	 	`, `El Centro de Operaciones de Emergencia del Fenómeno de El Niño ha permitido mejorar una respuesta ante El Niño.		`],
+    }
+    ,
+    {
+      imagen: `` ,
+      pregunta: `20.	Lee el siguiente fragmento nuevamente: Corresponde al Gobierno Central, además, otorgar la asistencia técnica necesaria para que esos recursos no reviertan al tesoro v se utilicen en atender a la población frente a las emergencias." .......................¿QUÉ OPINAS SOBRE LAS IDEAS DE ESTE FRAGMENTO DEL TEXTO? `,
+      respuesta: `Está bien que, ante la dificultad de ejecución presupuestal en las regiones, el gobierno central debe dar orientaciones.			`,
+      distractores: [`Creo que en las regiones hay muy buenos profesionales; pero, no implementan planes de desarrollo ante emergencias.			`, `	Me parece que las autoridades regionales son muy buenos optimizando el uso de los recursos asignados por el gobierno.		`, `	Está muy mal que los gobiernos regionales de todo el país puedan pedir ayudar de cómo realizar gastos en las emergencias.			`],
+    }
+ 
+  ];
